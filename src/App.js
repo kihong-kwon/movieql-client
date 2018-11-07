38f3df44ab9124f920fbf9dd592d1b748d0ef1e3
kihong-kwon <kihong.kwon7389@gmail.com>
2018-11-08 00:40:58 +0900

    #2 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './applloClient';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App"></div>
+      </ApolloProvider>
     );
   }
 }
